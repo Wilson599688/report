50e8287dabd4d3e58221c961cf5f1cddae996124
Wilson599688 <168523014+Wilson599688@users.noreply.github.com>
2024-06-04 14:30:55 +0800

    Add files via upload

--- Shioaji_KBar_MA_RSI__BollingerBand_MACD_backTest_load_Excel_and_Draw_KBar_2_plotly_chooseDates_streamlit_GitHub_6_short.py
+++ Shioaji_KBar_MA_RSI__BollingerBand_MACD_backTest_load_Excel_and_Draw_KBar_2_plotly_chooseDates_streamlit_GitHub_6_short.py
@@ -1,114 +1,129 @@
 # 載入必要模組
 import os
-# os.chdir(r'C:\Users\user\Dropbox\系務\專題實作\112\金融看板\for students')
 #import haohaninfo
 #from order_Lo8 import Record
 import numpy as np
 #from talib.abstract import SMA,EMA, WMA, RSI, BBANDS, MACD
 #import sys
-# import indicator_f_Lo2_short,datetime, indicator_forKBar_short
-import datetime
+import indicator_f_Lo2_short,datetime, indicator_forKBar_short
 import pandas as pd
 import streamlit as st 
 import streamlit.components.v1 as stc 
+from order_streamlit import Record
+import matplotlib.pyplot as plt
+import matplotlib
 
 
-###### (1) 開始設定 ######
+####### (1) 開始設定 #######
+###### 設定網頁標題介面 
 html_temp = """
-		<div style="background-color:#3872fb;padding:10px;border-radius:10px">
-		<h1 style="color:white;text-align:center;">金融資料視覺化呈現 (金融看板) </h1>
-		<h2 style="color:white;text-align:center;">Financial Dashboard </h2>
+		<div style="background-color:#3872fb;padding:10px;border-radius:10px">   
+		<h1 style="color:white;text-align:center;">金融看板與程式交易平台 </h1>
+		<h2 style="color:white;text-align:center;">Financial Dashboard and Program Trading </h2>
 		</div>
 		"""
 stc.html(html_temp)
 
-#df = pd.read_excel("kbars_台積電_1100701_1100708_2.xlsx")
-#df = pd.read_excel("kbars_2330_2022-07-01-2022-07-31.xlsx")
 
-# ## 讀取 excel 檔
+###### 讀取資料
+@st.cache_data(ttl=3600, show_spinner="正在加載資料...")  ## Add the caching decorator
+def load_data(path):
+    df = pd.read_pickle(path)
+    return df
+# ##### 讀取 excel 檔
 # df_original = pd.read_excel("kbars_2330_2022-01-01-2022-11-18.xlsx")
 
-# ## 保存为Pickle文件:
-# df_original.to_pickle('kbars_2330_2022-01-01-2022-11-18.pkl')
 
-## 读取Pickle文件
-df_original = pd.read_pickle('kbars_2330_2022-01-01-2022-11-18.pkl')
+###### 選擇金融商品
+st.subheader("選擇金融商品: ")
+# choices = ['台積電: 2022.1.1 至 2024.4.9', '大台指2024.12到期: 2024.1 至 2024.4.9']
+choices = ['台積電: 2022.1.1 至 2024.4.9', '大台指期貨2024.12到期: 2023.12 至 2024.4.11', '小台指期貨2024.12到期: 2023.12 至 2024.4.11', '英業達2020.1.2 至 2024.4.12', '堤維西2020.1.2 至 2024.4.12']
+choice = st.selectbox('選擇金融商品', choices, index=0)
+##### 读取Pickle文件
+if choice == '台積電: 2022.1.1 至 2024.4.9':
+    df_original = load_data('kbars_2330_2022-01-01-2024-04-09.pkl')
+    # df_original = load_data('kbars_2330_2022-01-01-2024-04-09.pkl')
+    # df_original = load_data('kbars_2330_2022-01-01-2022-11-18.pkl')  
+    # df_original = pd.read_pickle('kbars_2330_2022-01-01-2022-11-18.pkl')
+    #df.columns  ## Index(['Unnamed: 0', 'time', 'open', 'low', 'high', 'close', 'volume','amount'], dtype='object')
+    # df_original = df_original.drop('Unnamed: 0',axis=1)
+# if choice == '大台指2024.12到期: 2024.1 至 2024.4.9':
+#     df_original = load_data('kbars_TXF202412_2024-01-01-2024-04-09.pkl')  
+if choice == '大台指期貨2024.12到期: 2023.12 至 2024.4.11':
+    df_original = load_data('kbars_TXF202412_2023-12-21-2024-04-11.pkl')
+if choice == '小台指期貨2024.12到期: 2023.12 至 2024.4.11':
+    df_original = load_data('kbars_MXF202412_2023-12-21-2024-04-11.pkl')
+if choice == '英業達2020.1.2 至 2024.4.12':
+    df_original = load_data('kbars_2356_2020-01-01-2024-04-12.pkl')
+if choice == '堤維西2020.1.2 至 2024.4.12':
+    df_original = load_data('kbars_1522_2020-01-01-2024-04-12.pkl')
 
 
-#df.columns  ## Index(['Unnamed: 0', 'time', 'open', 'low', 'high', 'close', 'volume','amount'], dtype='object')
-df_original = df_original.drop('Unnamed: 0',axis=1)
-#df.columns  ## Index(['time', 'open', 'low', 'high', 'close', 'volume', 'amount'], dtype='object')
-#df['time']
-#type(df['time'])  ## pandas.core.series.Series
-#df['time'][11]
-#df.head()
-#df.tail()
-#type(df['time'][0])
 
 
-##### 選擇資料區間
-st.subheader("選擇開始與結束的日期, 區間:2022-01-03 至 2022-11-18")
-start_date = st.text_input('選擇開始日期 (日期格式: 2022-01-03)', '2022-01-03')
-end_date = st.text_input('選擇結束日期 (日期格式: 2022-11-18)', '2022-11-18')
+###### 選擇資料區間
+st.subheader("選擇資料時間區間")
+if choice == '台積電: 2022.1.1 至 2024.4.9':
+    start_date = st.text_input('輸入開始日期(日期格式: 2022-01-01), 區間:2022-01-01 至 2024-04-09', '2022-01-01')
+    end_date = st.text_input('輸入結束日期 (日期格式: 2024-04-09), 區間:2022-01-01 至 2024-04-09', '2024-04-09')
+if choice == '大台指期貨2024.12到期: 2023.12 至 2024.4.11':
+    start_date = st.text_input('輸入開始日期(日期格式: 2023-12-21), 區間:2023-12-21 至 2024-04-11', '2023-12-21')
+    end_date = st.text_input('輸入結束日期 (日期格式: 2024-04-11), 區間:2023-12-21 至 2024-04-11', '2024-04-11')
+if choice == '小台指期貨2024.12到期: 2023.12 至 2024.4.11':
+    start_date = st.text_input('輸入開始日期(日期格式: 2023-12-21), 區間:2023-12-21 至 2024-04-11', '2023-12-21')
+    end_date = st.text_input('輸入結束日期 (日期格式: 2024-04-11), 區間:2023-12-21 至 2024-04-11', '2024-04-11')
+if choice == '英業達2020.1.2 至 2024.4.12':
+    start_date = st.text_input('輸入開始日期(日期格式: 2020-01-02), 區間:2020-01-02 至 2024-04-12', '2020-01-02')
+    end_date = st.text_input('輸入結束日期 (日期格式: 2024-04-12), 區間:2020-01-02 至 2024-04-12', '2024-04-12')
+if choice == '堤維西2020.1.2 至 2024.4.12':
+    start_date = st.text_input('輸入開始日期(日期格式: 2020-01-02), 區間:2020-01-02 至 2024-04-12', '2020-01-02')
+    end_date = st.text_input('輸入結束日期 (日期格式: 2024-04-12), 區間:2020-01-02 至 2024-04-12', '2024-04-12')
+
+
+
 start_date = datetime.datetime.strptime(start_date,'%Y-%m-%d')
 end_date = datetime.datetime.strptime(end_date,'%Y-%m-%d')
 # 使用条件筛选选择时间区间的数据
 df = df_original[(df_original['time'] >= start_date) & (df_original['time'] <= end_date)]
 
 
-###### (2) 轉化為字典 ######:
-KBar_dic = df.to_dict()
-#type(KBar_dic)
-#KBar_dic.keys()  ## dict_keys(['time', 'open', 'low', 'high', 'close', 'volume', 'amount'])
-#KBar_dic['open']
-#type(KBar_dic['open'])  ## dict
-#KBar_dic['open'].values()
-#type(KBar_dic['open'].values())  ## dict_values
-KBar_open_list = list(KBar_dic['open'].values())
-KBar_dic['open']=np.array(KBar_open_list)
-#type(KBar_dic['open'])  ## numpy.ndarray
-#KBar_dic['open'].shape  ## (1596,)
-#KBar_dic['open'].size   ##  1596
+####### (2) 轉化為字典 #######
+@st.cache_data(ttl=3600, show_spinner="正在加載資料...")  ## Add the caching decorator
+def To_Dictionary_1(df):
+    KBar_dic = df.to_dict()
+    KBar_open_list = list(KBar_dic['open'].values())
+    KBar_dic['open']=np.array(KBar_open_list)
+    
+    KBar_dic['product'] = np.repeat('tsmc', KBar_dic['open'].size)
+    #KBar_dic['product'].size   ## 1596
+    #KBar_dic['product'][0]      ## 'tsmc'
+    
+    KBar_time_list = list(KBar_dic['time'].values())
+    KBar_time_list = [i.to_pydatetime() for i in KBar_time_list] ## Timestamp to datetime
+    KBar_dic['time']=np.array(KBar_time_list)
+    
+    KBar_low_list = list(KBar_dic['low'].values())
+    KBar_dic['low']=np.array(KBar_low_list)
+    
+    KBar_high_list = list(KBar_dic['high'].values())
+    KBar_dic['high']=np.array(KBar_high_list)
+    
+    KBar_close_list = list(KBar_dic['close'].values())
+    KBar_dic['close']=np.array(KBar_close_list)
+    
+    KBar_volume_list = list(KBar_dic['volume'].values())
+    KBar_dic['volume']=np.array(KBar_volume_list)
+    
+    KBar_amount_list = list(KBar_dic['amount'].values())
+    KBar_dic['amount']=np.array(KBar_amount_list)
+    
+    return KBar_dic
 
-KBar_dic['product'] = np.repeat('tsmc', KBar_dic['open'].size)
-#KBar_dic['product'].size   ## 1596
-#KBar_dic['product'][0]      ## 'tsmc'
+KBar_dic = To_Dictionary_1(df)
 
-KBar_time_list = list(KBar_dic['time'].values())
-KBar_time_list = [i.to_pydatetime() for i in KBar_time_list] ## Timestamp to datetime
-KBar_dic['time']=np.array(KBar_time_list)
-
-# KBar_time_list[0]        ## Timestamp('2022-07-01 09:01:00')
-# type(KBar_time_list[0])  ## pandas._libs.tslibs.timestamps.Timestamp
-#KBar_time_list[0].to_pydatetime() ## datetime.datetime(2022, 7, 1, 9, 1)
-#KBar_time_list[0].to_numpy()      ## numpy.datetime64('2022-07-01T09:01:00.000000000')
-#KBar_dic['time']=np.array(KBar_time_list)
-#KBar_dic['time'][80]   ## Timestamp('2022-09-01 23:02:00')
-
-KBar_low_list = list(KBar_dic['low'].values())
-KBar_dic['low']=np.array(KBar_low_list)
-
-KBar_high_list = list(KBar_dic['high'].values())
-KBar_dic['high']=np.array(KBar_high_list)
-
-KBar_close_list = list(KBar_dic['close'].values())
-KBar_dic['close']=np.array(KBar_close_list)
-
-KBar_volume_list = list(KBar_dic['volume'].values())
-KBar_dic['volume']=np.array(KBar_volume_list)
-
-KBar_amount_list = list(KBar_dic['amount'].values())
-KBar_dic['amount']=np.array(KBar_amount_list)
 
 
 ######  (3) 改變 KBar 時間長度 (以下)  ########
-# Product_array = np.array([])
-# Time_array = np.array([])
-# Open_array = np.array([])
-# High_array = np.array([])
-# Low_array = np.array([])
-# Close_array = np.array([])
-# Volume_array = np.array([])
 
 Date = start_date.strftime("%Y-%m-%d")
 
@@ -236,20 +251,61 @@ KBar_df['RSI_Middle']=np.array([50]*len(KBar_dic['time']))
 last_nan_index_RSI = KBar_df['RSI_long'][::-1].index[KBar_df['RSI_long'][::-1].apply(pd.isna)][0]
 
 
-# #### 逆勢策略
-# ### 建立部位管理物件
-# OrderRecord=Record() 
-# ### 計算 RSI指標, 天花板與地板
-# RSIPeriod=5
-# Ceil=80
-# Floor=20
-# MoveStopLoss=30
-# KBar_dic['RSI']=RSI(KBar_dic,timeperiod=RSIPeriod)
-# KBar_dic['Ceil']=np.array([Ceil]*len(KBar_dic['time']))
-# KBar_dic['Floor']=np.array([Floor]*len(KBar_dic['time']))
+######  (iii) Bollinger Band (布林通道) 策略 
+##### 假设df是包含价格数据的Pandas DataFrame，'close'列是每日收盘价格
+@st.cache_data(ttl=3600, show_spinner="正在加載資料...")  ## Add the caching decorator
+def Calculate_Bollinger_Bands(df, period=20, num_std_dev=2):
+    df['SMA'] = df['close'].rolling(window=period).mean()
+    df['Standard_Deviation'] = df['close'].rolling(window=period).std()
+    df['Upper_Band'] = df['SMA'] + (df['Standard_Deviation'] * num_std_dev)
+    df['Lower_Band'] = df['SMA'] - (df['Standard_Deviation'] * num_std_dev)
+    return df
 
-# ### 將K線 Dictionary 轉換成 Dataframe
-# KBar_RSI_df=pd.DataFrame(KBar_dic)
+
+#####  設定布林通道(Bollinger Band)相關參數:
+with st.expander("設定布林通道(Bollinger Band)相關參數:"):
+    # st.subheader("設定計算布林通道(Bollinger Band)上中下三通道之K棒週期數目(整數, 例如 20)")
+    period = st.slider('設定計算布林通道(Bollinger Band)上中下三通道之K棒週期數目(整數, 例如 20)', 0, 100, 20, key='BB_period')
+    # st.subheader("設定計算布林通道(Bollinger Band)上中(或下中)通道之帶寬(例如 2 代表上中通道寬度為2倍的標準差)")
+    num_std_dev = st.slider('設定計算布林通道(Bollinger Band)上中(或下中)通道之帶寬(例如 2 代表上中通道寬度為2倍的標準差)', 0, 100, 2, key='BB_heigh')
+
+##### 計算布林通道上中下通道:
+KBar_df = Calculate_Bollinger_Bands(KBar_df, period, num_std_dev)
+
+##### 尋找最後 NAN值的位置
+last_nan_index_BB = KBar_df['SMA'][::-1].index[KBar_df['SMA'][::-1].apply(pd.isna)][0]
+
+######  (iv) MACD(異同移動平均線) 策略 
+# 假设df是包含价格数据的Pandas DataFrame，'price'列是每日收盘价格
+@st.cache_data(ttl=3600, show_spinner="正在加載資料...")  ## Add the caching decorator
+def Calculate_MACD(df, fast_period=12, slow_period=26, signal_period=9):
+    df['EMA_Fast'] = df['close'].ewm(span=fast_period, adjust=False).mean()
+    df['EMA_Slow'] = df['close'].ewm(span=slow_period, adjust=False).mean()
+    df['MACD'] = df['EMA_Fast'] - df['EMA_Slow']  ## DIF
+    df['Signal_Line'] = df['MACD'].ewm(span=signal_period, adjust=False).mean()   ## DEA或信號線
+    df['MACD_Histogram'] = df['MACD'] - df['Signal_Line']  ## MACD = DIF-DEA
+    return df
+
+#####  設定MACD三種週期的K棒長度:
+with st.expander("設定MACD三種週期的K棒長度:"):
+    # st.subheader("設定計算 MACD的快速線週期(例如 12根日K)")
+    fast_period = st.slider('設定計算 MACD快速線的K棒週期數目(例如 12根日K)', 0, 100, 12, key='visualization_MACD_quick')
+    # st.subheader("設定計算 MACD的慢速線週期(例如 26根日K)")
+    slow_period = st.slider('設定計算 MACD慢速線的K棒週期數目(例如 26根日K)', 0, 100, 26, key='visualization_MACD_slow')
+    # st.subheader("設定計算 MACD的訊號線週期(例如 9根日K)")
+    signal_period = st.slider('設定計算 MACD訊號線的K棒週期數目(例如 9根日K)', 0, 100, 9, key='visualization_MACD_signal')
+
+##### 計算MACD:
+KBar_df = Calculate_MACD(KBar_df, fast_period, slow_period, signal_period)
+
+##### 尋找最後 NAN值的位置
+# last_nan_index_MACD = KBar_df['MACD'][::-1].index[KBar_df['MACD'][::-1].apply(pd.isna)][0]
+#### 試著找出最後一個 NaN 值的索引，但在這之前要檢查是否有 NaN 值
+nan_indexes_MACD = KBar_df['MACD'][::-1].index[KBar_df['MACD'][::-1].apply(pd.isna)]
+if len(nan_indexes_MACD) > 0:
+    last_nan_index_MACD = nan_indexes_MACD[0]
+else:
+    last_nan_index_MACD = 0
 
 
 ###### (5) 將 Dataframe 欄位名稱轉換  ###### 
@@ -304,17 +360,43 @@ with st.expander("K線圖, 長短 RSI"):
     st.plotly_chart(fig2, use_container_width=True)
 
 
+###### K線圖, Bollinger Band    
+with st.expander("K線圖,布林通道"):
+    fig3 = make_subplots(specs=[[{"secondary_y": True}]])
+    fig3.add_trace(go.Candlestick(x=KBar_df['time'],
+                    open=KBar_df['open'], high=KBar_df['high'],
+                    low=KBar_df['low'], close=KBar_df['close'], name='K線'),
+                    secondary_y=True)    
+    fig3.add_trace(go.Scatter(x=KBar_df['time'][last_nan_index_BB+1:], y=KBar_df['SMA'][last_nan_index_BB+1:], mode='lines',line=dict(color='black', width=2), name='布林通道中軌道'), 
+                  secondary_y=False)
+    fig3.add_trace(go.Scatter(x=KBar_df['time'][last_nan_index_BB+1:], y=KBar_df['Upper_Band'][last_nan_index_BB+1:], mode='lines',line=dict(color='red', width=2), name='布林通道上軌道'), 
+                  secondary_y=False)
+    fig3.add_trace(go.Scatter(x=KBar_df['time'][last_nan_index_BB+1:], y=KBar_df['Lower_Band'][last_nan_index_BB+1:], mode='lines',line=dict(color='blue', width=2), name='布林通道下軌道'), 
+                  secondary_y=False)
+    
+    fig3.layout.yaxis2.showgrid=True
+
+    st.plotly_chart(fig3, use_container_width=True)
 
 
 
-
-
-
-
-
-
-
-
-
-
+###### MACD
+with st.expander("MACD(異同移動平均線)"):
+    fig4 = make_subplots(specs=[[{"secondary_y": True}]])
+    
+    # #### include candlestick with rangeselector
+    # fig4.add_trace(go.Candlestick(x=KBar_df['Time'],
+    #                 open=KBar_df['Open'], high=KBar_df['High'],
+    #                 low=KBar_df['Low'], close=KBar_df['Close'], name='K線'),
+    #                secondary_y=True)   ## secondary_y=True 表示此圖形的y軸scale是在右邊而不是在左邊
+    
+    #### include a go.Bar trace for volumes
+    fig4.add_trace(go.Bar(x=KBar_df['time'], y=KBar_df['MACD_Histogram'], name='MACD Histogram', marker=dict(color='black')),secondary_y=False)  ## secondary_y=False 表示此圖形的y軸scale是在左邊而不是在右邊
+    fig4.add_trace(go.Scatter(x=KBar_df['time'][last_nan_index_MACD+1:], y=KBar_df['Signal_Line'][last_nan_index_MACD+1:], mode='lines',line=dict(color='orange', width=2), name='訊號線(DEA)'), 
+                  secondary_y=True)
+    fig4.add_trace(go.Scatter(x=KBar_df['time'][last_nan_index_MACD+1:], y=KBar_df['MACD'][last_nan_index_MACD+1:], mode='lines',line=dict(color='pink', width=2), name='DIF'), 
+                  secondary_y=True)
+    
+    fig4.layout.yaxis2.showgrid=True
+    st.plotly_chart(fig4, use_container_width=True)
 
